e2a09e9a26763cd635a7118ce3cb3cf1f83ae559
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-03 12:01:45 +0530

    fixed bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,7 @@ const registerUser=asyncHandler(async(req,res)=>{
   if([fullname,email,username,password].some((field)=>field.trim()==="")){
     throw new ApiError(400,"All fiellds are required")
   }
-  const existedUser=User.findOne({
+  const existedUser=await User.findOne({
    $or:[{username},{email}]
 })
 if (existedUser){
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,6 +17,7 @@ const uploadOnCloudinary=async(localFilePath)=>{
         })
         //file has been uploaded successfully
         console.log("file is uploaded on cloudinary",response.url)
+        // fs.unlinkSync(localFilePath)
         return response
     }
     catch(error){
