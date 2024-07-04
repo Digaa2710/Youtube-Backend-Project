ffec84e0a95d5e951030d0aadf974ae0ec3d554d
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-04 08:19:26 +0530

    fixed bugs

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,6 +4,7 @@ const storage=multer.diskStorage({
     destination:function(req,file,cb){
         //destination
         cb(null,"./public/temp")
+        //cb =callback function
     },
     filename:function(req,file,cb){
         
