891abd19ca3d096fc4a1551cc6f131fa41e2e84b
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-06 13:14:25 +0530

    MongoDB aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -317,7 +317,75 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
       new ApiResponse(200,user,"Cover image updated successfully ")
    )
       
-   
+
+   })
+
+   const getUserChannelProfile=asyncHandler(async(req,res)=>{
+
+      const{username}=req.params
+      if(!username?.trim()){
+         throw new ApiError(400,"username is missing")
+      }
+     const channel= await User.aggregate([
+      {
+         $match:{
+            username:username?.toLowerCase()
+         }
+      },
+      {
+         $lookup:{
+            from:"subscription",
+            localField:"_id",
+            foreignField:"channel",
+            as:"subscribers"
+         }
+      },
+      {
+         $lookup:{
+            from:"subscription",
+            localField:"_id",
+            foreignField:"subscriber",
+            as:"subscribedTo"
+         }
+      },
+      {
+         $addFields:{
+            subscribersCount:{
+               $size:"$subscribers"
+            },
+            channelsSubscribedToCount:{
+               $size:"subscribedTo"
+            },
+            isSubscribed:{
+               $cond:{
+                  if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                  then:true,
+                  else:false
+               }
+            }
+         }
+      },{
+         $project:{
+            fullname:1,
+            username:1,
+            subscribersCount:1,
+            channelsSubscribedToCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1,
+            email:1
+         }
+      }
+     ])
+     
+     if(!channel?.length){
+      throw new ApiError(404,'Channel does not exist')
+     }
+
+     return res
+     .status(200)
+     .json(new ApiResponse(200,channel[0],"user channel fetched successfully"))
+
    })
 
 
