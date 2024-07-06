415f038da94f548b25c31c586c10c5502721f010
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-06 10:04:15 +0530

    Added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,12 @@
+import mongoose,{Schema} from "mongoose"
+
+const subscriptionSchema=new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    }
+},{timestamps:true})
